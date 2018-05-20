3dc1d1bb8bf7993091480471c737d8739433a197
j-piecuch <j.piecuch96@gmail.com>
2018-05-20 16:31:46 +0200

    Move pcpu and code from ebase.S and exc.S to their own sections

--- sys/pcpu.c
+++ sys/pcpu.c
@@ -1,7 +1,8 @@
 #include <thread.h>
 #include <pcpu.h>
+#include <common.h>
 
-pcpu_t _pcpu_data[1] = {{}};
+pcpu_t __section(".pcpu") _pcpu_data[1] = {{}};
 
 static thread_t *dummy =
   &(thread_t){.td_name = "dummy thread", .td_tid = 0, .td_idnest = 1};
