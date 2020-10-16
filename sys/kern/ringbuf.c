cb634fe461a5922622c96869dfa8d642fcb4e075
Jakub Piecuch <j.piecuch96@gmail.com>
2020-10-16 13:46:54 +0200

    Make more ringbuf functions inlinable.

--- sys/kern/ringbuf.c
+++ sys/kern/ringbuf.c
@@ -10,40 +10,6 @@ void ringbuf_init(ringbuf_t *rb, void *buf, size_t size) {
   rb->data = buf;
 }
 
-static void produce(ringbuf_t *buf, unsigned bytes) {
-  assert(buf->count + bytes <= buf->size);
-  assert(buf->head + bytes <= buf->size);
-  buf->count += bytes;
-  buf->head += bytes;
-  if (buf->head == buf->size)
-    buf->head = 0;
-}
-
-static void consume(ringbuf_t *buf, unsigned bytes) {
-  assert(buf->count >= bytes);
-  assert(buf->tail + bytes <= buf->size);
-  buf->count -= bytes;
-  buf->tail += bytes;
-  if (buf->tail == buf->size)
-    buf->tail = 0;
-}
-
-bool ringbuf_putb(ringbuf_t *buf, uint8_t byte) {
-  if (buf->count == buf->size)
-    return false;
-  buf->data[buf->head] = byte;
-  produce(buf, 1);
-  return true;
-}
-
-bool ringbuf_getb(ringbuf_t *buf, uint8_t *byte_p) {
-  if (buf->count == 0)
-    return false;
-  *byte_p = buf->data[buf->tail];
-  consume(buf, 1);
-  return true;
-}
-
 int ringbuf_read(ringbuf_t *buf, uio_t *uio) {
   assert(uio->uio_op == UIO_READ);
   /* repeat when used space is split into two parts */
